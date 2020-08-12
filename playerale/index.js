befb14d45296ae88b2e840ce12ed6776978c4331
bosslibra <alessandr.libralesso@studio.unibo.it>
2020-08-12 12:53:23 +0200

    Change directory system and answer template fetching
    Put images and aswer template in new directories, added jquery and create answer template for multiple choice questions

--- playerale/index.js
+++ playerale/index.js
@@ -1,15 +1,18 @@
-
-
-
-function replaceText(new_text) {
-    document.getElementByClassName("quiz_text").innerHTML = "";
-    document.getElementByClassName("quiz_text").innerHTML = new_text;
-}
-
 function inputTemplate() {
-    var element = document.getElementById("demo");
-    element.remove()
-
-    document.getElementsByClassName("quiz_answer")[0].innerHTML += "<button >Vero</button> <button >Falso</button>"
-
+  var element = document.getElementById("demo");
+  element.remove()
+  var value = "multipleChoice";
+  //value sarà una variabile da recuperare dal json della domanda, ha ora un valore impostato per scopi di testing
+  switch(String(value)) {
+      case "trueOrFalse":
+          document.getElementsByClassName("quiz_answer")[0].innerHTML += "<button >Vero</button> <button >Falso</button>"
+        break;
+      case "multipleChoice":
+        $(document).ready(()=>{
+          $("#y").load("./answer_template/multiple_choice_template.html");
+        })
+        break;
+      default:
+          document.getElementsByClassName("quiz_answer")[0].innerHTML += "Errore nel caricamento della domanda"
+  }
 }
