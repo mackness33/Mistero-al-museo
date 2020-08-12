0db573b96c62f23046865a50c0cf7e136ce6b704
elenalippolis <56910933+elenalippolis@users.noreply.github.com>
2020-08-12 19:28:06 +0200

    Add files via upload

---
+++ TWprj-chat/funzioni.js
@@ -0,0 +1,10 @@
+
+function chatOpen(){
+
+  var x= document.getElementById("chat-open");
+  if(x.style.visibility== "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
