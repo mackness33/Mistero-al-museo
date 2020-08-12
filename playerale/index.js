3d35ba875e28e9a2a9d3471bf6a0e30ad3fb6630
bosslibra <alessandr.libralesso@studio.unibo.it>
2020-08-12 18:06:59 +0200

    Add external file for true or false template

--- playerale/index.js
+++ playerale/index.js
@@ -1,11 +1,13 @@
 function inputTemplate() {
   var element = document.getElementById("demo");
   element.remove()
-  var value = "multipleChoice";
+  var value = "trueOrFalse";
   //value sarà una variabile da recuperare dal json della domanda, ha ora un valore impostato per scopi di testing
   switch(String(value)) {
       case "trueOrFalse":
-          document.getElementsByClassName("quiz_answer")[0].innerHTML += "<button >Vero</button> <button >Falso</button>"
+        $(document).ready(()=>{
+          $("#y").load("./answer_template/true_or_false_template.html");
+        })
         break;
       case "multipleChoice":
         $(document).ready(()=>{
