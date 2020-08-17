209b76feb6c06d9464a0f23022ae85934b178677
Mattia <mattiababbini99@gmail.com>
2020-08-17 08:01:23 +0200

    migliorie generali

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+require("dotenv").config(); //.env file (dev only)
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 //logger
 const logger = require("log4js").getLogger();
-logger.level = "info";
+logger.level = process.env.LOG_LEVEL;
 
 let api = require("./routes/api");
 
+app.use(express.json());
 app.use("/api/v1", api);
 
 app.listen(port, () => {
