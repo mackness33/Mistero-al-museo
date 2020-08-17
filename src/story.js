73a7ef9b3c2e2ce2fc335b6ac6bdea62538bcef7
Mattia <mattiababbini99@gmail.com>
2020-08-17 08:01:49 +0200

    semplificate solo per comodità

--- src/story.js
+++ src/story.js
@@ -6,142 +6,15 @@ let story =[{
     "desc": "quiz description",
     "style":"url css",
     "age":"8-10",
-    "team":false,
-    "missions": [
-        {
-            "ID": 1,
-            "title": "mission title",
-            "desc": "mission desc",
-            "tasks": [
-                {
-                    "ID": 1,
-                    "title": "task title",
-                    "desc": "task desc",
-                    "img":"URL img",
-                    "answers":[
-                        {
-                            "ID":1,
-                            "desc":"answer text1",
-                            "right":true
-                        },
-                        {
-                            "ID":2,
-                            "desc":"answer text2",
-                            "right":false
-                        },
-                        {
-                            "ID":3,
-                            "desc":"answer text3",
-                            "right":false
-                        },
-                        {
-                            "ID":4,
-                            "desc":"answer text4",
-                            "right":false
-                        }
-
-
-                    ]
-                },
-                {
-                    "ID": 2,
-                    "title": "task title2",
-                    "desc": "task desc2",
-                    "img":"URL img2",
-                    "answers":[
-                        {
-                            "ID":1,
-                            "desc":"answer text1",
-                            "right":true
-                        },
-                        {
-                            "ID":2,
-                            "desc":"answer text2",
-                            "right":false
-                        },
-                        {
-                            "ID":3,
-                            "desc":"answer text3",
-                            "right":false
-                        },
-                        {
-                            "ID":4,
-                            "desc":"answer text4",
-                            "right":false
-                        }
-
-
-                    ]
-                }
-            ]
-        },
-        {
-            "ID": 2,
-            "title": "mission title",
-            "desc": "mission desc",
-            "tasks": [
-                {
-                    "ID": 1,
-                    "title": "task title",
-                    "desc": "task desc",
-                    "img":"URL img",
-                    "answers":[
-                        {
-                            "ID":1,
-                            "desc":"answer text1",
-                            "right":true
-                        },
-                        {
-                            "ID":2,
-                            "desc":"answer text2",
-                            "right":false
-                        },
-                        {
-                            "ID":3,
-                            "desc":"answer text3",
-                            "right":false
-                        },
-                        {
-                            "ID":4,
-                            "desc":"answer text4",
-                            "right":false
-                        }
-
-
-                    ]
-                },
-                {
-                    "ID": 2,
-                    "title": "task title2",
-                    "desc": "task desc2",
-                    "img":"URL img2",
-                    "answers":[
-                        {
-                            "ID":1,
-                            "desc":"answer text1",
-                            "right":true
-                        },
-                        {
-                            "ID":2,
-                            "desc":"answer text2",
-                            "right":false
-                        },
-                        {
-                            "ID":3,
-                            "desc":"answer text3",
-                            "right":false
-                        },
-                        {
-                            "ID":4,
-                            "desc":"answer text4",
-                            "right":false
-                        }
-
-
-                    ]
-                }
-            ]
-        }
-    ]
-}]
+    "team":false    
+},
+{
+    "ID": "AOVMBTP23PF",
+    "title": "Quiz title",
+    "desc": "quiz description",
+    "style":"url css",
+    "age":"8-10",
+    "team":false    
+}
+]
 module.exports = story;
