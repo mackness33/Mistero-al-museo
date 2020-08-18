68dd8d40d77bb68eeb1d5d451d57592235f313a7
bosslibra <alessandr.libralesso@studio.unibo.it>
2020-08-18 18:34:00 +0200

    initial commit
    Add basic version of button functions for answer control

--- playerale/index.js
+++ playerale/index.js
@@ -1,6 +1,8 @@
+var toBeChecked = "";//variabile da controllare attraverso il tasto Controlla
+
 function inputTemplate() {
   var element = document.getElementById("demo");
-  element.remove()
+  
   var value = "trueOrFalse";
   //value sarà una variabile da recuperare dal json della domanda, ha ora un valore impostato per scopi di testing
   switch(String(value)) {
@@ -15,6 +17,29 @@ function inputTemplate() {
         })
         break;
       default:
-          document.getElementsByClassName("quiz_answer")[0].innerHTML += "Errore nel caricamento della domanda"
+          document.getElementsByClassName("quiz_answer")[0].innerHTML += "Errore nel caricamento della domanda";
+  }
+}
+
+//funzione che aggiorna il valore della variabile toBeChecked, la quale è una stringa
+//prende in input una stringa dal click dei tasti vero o falso.
+//Nel caso del bottone Vero l'input è "true", altrimenti è "false"
+function trueOrFalseClick(answer){
+  toBeChecked = answer;
+}
+
+//prende in input un numero che dipende da quale risposta è stata selezionata
+//prima di aggiornare la variabile toBeChecked, rende il numero una stringa
+function multipleChoiceClick(answer){
+  toBeChecked = string(answer);
+}
+
+function control() {
+  if (toBeChecked == "true"){
+    document.getElementById("error").innerHTML = "Anwered true";
+  } else if (toBeChecked == "false"){
+    document.getElementById("error").innerHTML = "Anwered false";
+  } else {
+    document.getElementById("error").innerHTML = "no answer";
   }
 }
