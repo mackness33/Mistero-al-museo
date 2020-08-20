5b48c85977c8c594e1a68f6ba6f67b3c2935e57d
bosslibra <alessandr.libralesso@studio.unibo.it>
2020-08-20 19:55:04 +0200

    Change template implementation
    Sono passato da fare un load di html locale a usare display: block e display:none per mostrare il template giusto per le risposte

--- playerale/index.js
+++ playerale/index.js
@@ -21,6 +21,25 @@ function inputTemplate() {
   }
 }
 
+//prende in input il tipo di template necessario per la risposta
+//cambia il display dei div in modo tale da mostrare solo quello necessario
+function templateSwitcher(){
+  $('.answer_template').hide(); //sets every template to display:none; probabilmente ridondante una volta che sarà fuori dal testing, in quanto già presente in desktop.css
+  var template = "multipleChoice"; //this variables is to be changed later, needs to take the string from JSON
+  switch(template){
+    case "trueOrFalse":
+      document.getElementById("trueOrFalse").style.display = "block";
+      break;
+    case "multipleChoice":
+      document.getElementById("multipleChoice").style.display = "block";
+      break;
+    default:
+      document.getElementById("error").innerHTML = "Errore nel caricare la selezione della risposta";
+      break;
+  }
+}
+
+
 //funzione che aggiorna il valore della variabile toBeChecked, la quale è una stringa
 //prende in input una stringa dal click dei tasti vero o falso.
 //Nel caso del bottone Vero l'input è "true", altrimenti è "false"
@@ -35,11 +54,15 @@ function multipleChoiceClick(answer){
 }
 
 function control() {
-  if (toBeChecked == "true"){
+  if (toBeChecked === "true"){
     document.getElementById("error").innerHTML = "Anwered true";
-  } else if (toBeChecked == "false"){
+  } else if (toBeChecked === "false"){
     document.getElementById("error").innerHTML = "Anwered false";
   } else {
     document.getElementById("error").innerHTML = "no answer";
   }
 }
+
+
+
+templateSwitcher();
