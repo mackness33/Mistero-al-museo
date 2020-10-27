bbfbcab10e5a80667415954b58394220ea6b71b4
mackness33 <davidantonhy962@gmail.com>
2020-10-27 19:41:40 +0100

    ⚡️ First save to mongo

--- src/database.js
+++ src/database.js
@@ -34,11 +34,26 @@ try {
   const User = mongoose.model('User', userSchema);
 
   // create a document
-   const first_user = new User({
-     name: { first: 'Axl', last: 'Rose' }
-   });
+  const first_user = new User({
+    _id: 1,
+    name: { first: 'Axl', last: 'Rose' }
+  });
 
-   logger.info(`Create the first document`);
+  logger.info(`Create the first document`);
+
+  first_user.save(function (err) {
+    if (err) return logger.info(`ERROR: ${err}`);
+    // saved!
+  });
+
+  // or
+
+  // Tank.create({ size: 'small' }, function (err, small) {
+  //   if (err) return handleError(err);
+  //   // saved!
+  // })
+  logger.info(`Save the first document`);
+
 
 } catch (error) {
   logger.info(`ERROR: ${error}`);
